1d454b336f3b588deb6848018a4180c19d72343a
Miroslav <138483137+MiroCoder@users.noreply.github.com>
2026-09-06 12:35:32 +0300

    Add Day 9 import demo

---
+++ main.py
@@ -0,0 +1,9 @@
+from helpers.utils import add, multiply
+from helpers import utils as u
+from services import get_user
+
+
+print(add(2, 3))
+print(multiply(4, 5))
+print(u.multiply(4, 5))
+print(get_user())
